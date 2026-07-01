bf87cbd6a5d85129b482c1bbb1e956d063fe95bf
vitoriavweb <124357303+vitoriavweb@users.noreply.github.com>
2026-07-01 19:48:10 -0300

    Delete plataforma_doacao_de_sangue directory

--- plataforma_doacao_de_sangue/db.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy as sa
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = sa.create_engine("sqlite:///doacoes.db")
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
